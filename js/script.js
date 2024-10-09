41041957e051c3726ef321bdb2c82a0f57d467a8
lucasoliveiraDEV22 <lucaszrones@gmail.com>
2024-10-09 14:45:25 -0300

    new updates

--- js/script.js
+++ js/script.js
@@ -1,50 +1,4 @@
-// const hiddenElements = document.querySelectorAll('main, section, footer');
-
-// const observer = new IntersectionObserver((entries) => {
-//   entries.forEach((entry) => {
-//     if (entry.isIntersecting) {
-//       entry.target.classList.add('show');
-//     } else {
-//       entry.target.classList.remove('show');
-//     }
-//   });
-// });
-
-// hiddenElements.forEach((el) => observer.observe(el));
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   const elements = document.querySelectorAll("main, section, footer");
-
-//   function checkScroll() {
-//     const triggerBottom = window.innerHeight / 5 * 4;
-
-//     elements.forEach((element) => {
-//       const elementTop = element.getBoundingClientRect().top;
-
-//       if (elementTop < triggerBottom) {
-//         element.classList.add("show");
-//       } else {
-//         element.classList.remove("show");
-//       }
-//     });
-//   }
-
-//   window.addEventListener("scroll", checkScroll);
-//   checkScroll(); // Executa ao carregar a página para verificar a posição inicial
-// });
-
-// document.addEventListener("scroll", function () {
-//   // Pega o valor da rolagem vertical
-//   let scrollY = window.scrollY;
-
-//   // Calcula o quanto os cards se moverão horizontalmente
-//   let horizontalScroll = scrollY * 0.5; // Ajuste o fator multiplicador conforme necessário
-
-//   // Aplica a transformação horizontal aos cards
-//   document.querySelector(".cards-container").style.transform = `translateX(-${horizontalScroll}px)`;
-// });
-
-/* --------------------Scroll-Animation---------------------- */
+/*--------------------Scroll-Animation---------------------- */
 
 const boxes = document.querySelectorAll('.about_text,.card-left, .card-right');
 
@@ -214,10 +168,5 @@ function nextSlide() {
     }
   });
   slides[(currentIndex + 1) % slides.length].checked = true;
-
-
-
-
-
 }
 
