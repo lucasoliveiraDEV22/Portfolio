1884bd449048d388f4feaaf67da09183bce18702
lucasoliveiraDEV22 <lucaszrones@gmail.com>
2024-10-08 22:27:58 -0300

    other updates

--- js/script.js
+++ js/script.js
@@ -192,3 +192,32 @@ const formSubmit = new FormSubmit({
 });
 formSubmit.init();
 
+/*-------------------LÓGICA DO CARROSSEL----------------- */
+
+function prevSlide() {
+  const slides = document.querySelectorAll('.slider input');
+  let currentIndex;
+  slides.forEach((slide, index) => {
+    if (slide.checked) {
+      currentIndex = index;
+    }
+  });
+  slides[(currentIndex - 1 + slides.length) % slides.length].checked = true;
+}
+
+function nextSlide() {
+  const slides = document.querySelectorAll('.slider input');
+  let currentIndex;
+  slides.forEach((slide, index) => {
+    if (slide.checked) {
+      currentIndex = index;
+    }
+  });
+  slides[(currentIndex + 1) % slides.length].checked = true;
+
+
+
+
+
+}
+
